7684cd04d29c6124480091ba681c8b95caefc708
shalevf6 <38422152+shalevf6@users.noreply.github.com>
2019-08-07 22:00:51 +0300

    - Some more fixes

--- Classifier.py
+++ Classifier.py
@@ -14,28 +14,28 @@ class Classifier:
     def build_model(self, train_raw_data):
         endURL = '\\train.csv'
         train_data = self.pre_process_data(train_raw_data)
-        self.__makeDiscretization(train_data)
-        self.__naiveBayes(train_data)
+        self.discretization(train_data)
+        self.build_NB_model(train_data)
 
     #the function is discretization and calc the binMinMax
-    def __makeDiscretization(self, dataStr):
+    def discretization(self, dataStr):
         struct = self.structure
         bins = self.num_of_bins
         labels = []
-        for label in range(self.num_bins):
+        for label in range(bins):
             labels.append(str(label))
         for (key, value) in struct.items():
-            if len(struct[key]['attributes'][0])==1 and struct[key]['attributes'][0] == "NUMERIC":
+            if len(struct[key]['attributes']) == 1 and struct[key]['attributes'][0] == "NUMERIC":
                 maximum = self.findMAX(dataStr, key)
                 minimum = self.findMIN(dataStr, key)
-                #the pace size
+                # the pace size
                 pace = self.calcPace(maximum, minimum, bins)
-                #calc by minmax formula
-                binMinMax = [float(sys.minint)] + \
+                # calc by minmax formula
+                binMinMax = [float('-inf')] + \
                                [(minimum + index * pace) for index in range(1, bins)] if 0 < pace else [minimum]
-                binMinMax += [float(sys.maxint)]
+                binMinMax += [float('inf')]
                 dataStr[key] = pd.cut(dataStr[key], bins=binMinMax, include_lowest=True, labels=labels)
-                struct[key]['attributes'][0] = binMinMax
+                struct[key]['attributes'] = binMinMax
 
     def findMAX(self, dataStr, attribute):
         return dataStr[attribute].max()
@@ -47,7 +47,7 @@ class Classifier:
         return (maximum - minimum) / bins
 
     def classify_input(self, test_raw_data):
-        test_data = self.preProcces4TestSet()
+        test_data = self.preProcces4TestSet(test_raw_data)
         endURL = '\\output.txt'
         with open(self.file_path + endURL, 'w') as outputFile:
             for index, record in test_data.iterrows():
@@ -56,11 +56,11 @@ class Classifier:
                 outputFile.write(row)
 
     def preProcces4TestSet(self, test_raw_data):
-        test_data = self.__insertData(test_raw_data)
-        self.__makeDiscretization(test_data)
+        test_data = self.pre_process_data(test_raw_data)
+        self.discretization(test_data)
         return test_data
 
-    def __naiveBayes(self, train_data):
+    def build_NB_model(self, train_data):
         struct = self.structure
         bins = self.num_of_bins
         probabilities = self.probabilities
@@ -70,16 +70,16 @@ class Classifier:
                 df = train_data.groupby([key, 'class']).size().reset_index(name='counts')
                 df.insert(3, "Prob", float(0.0), True)
 
-                if len(struct[key]['attributes'][0]) == 1 and struct[key]['attributes'][0] == "NUMERIC":
+                if len(struct[key]['attributes']) == 1 and struct[key]['attributes'][0] == "NUMERIC":
                     values = range(bins)
-                else: values = struct[key]['attributes'][0]
+                else: values = struct[key]['attributes']
                 for attVal in values:
-                    for classValue in struct['class']['attributes'][0]:
+                    for classValue in struct['class']['attributes']:
                         mergeAttributsWithClass = train_data[(train_data[key] == attVal) & (train_data["class"] == classValue)]
                         p = float(1) / len(values)
                         nc = len(mergeAttributsWithClass)
                         n = len(train_data[train_data["class"] == classValue])
-                        mEstimateValue = float(nc+2*p)/(n+2)
+                        mEstimateValue = float(nc + 2*p) / (n + 2)
                         the_Key = (key, attVal, classValue)
                         probabilities[the_Key] = mEstimateValue
 
@@ -87,7 +87,7 @@ class Classifier:
         struct = self.structure
         probabilities = self.probabilities
         probs = dict()
-        for Yes_No_Class in struct['class']['attributes'][0]:
+        for Yes_No_Class in struct['class']['attributes']:
             m_est_prob = 1
             for (key, val) in struct.items():
                 att_val = row_in_test[key]
